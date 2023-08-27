1dc1608a02a49a2e5eb0c35252175c742298f4a3
Will Garside <worgarside@gmail.com>
2023-08-27 14:06:22 +0100

    Set env var _before_ building

--- item_warehouse_website/run.sh
+++ item_warehouse_website/run.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/with-contenv bashio
 
-API_BASE_URL_IN=$(bashio::config 'api_base_url')
-
-
-if [[ -z "${API_BASE_URL-}" ]]
-then
-    export NEXT_PUBLIC_API_BASE_URL=${API_BASE_URL_IN}
+# If API_BASE_URL is set, use that. Otherwise, use the one from bashio config.
+if [[ -n "${API_BASE_URL-}" ]]; then
+    NEXT_PUBLIC_API_BASE_URL=${API_BASE_URL}
+else
+    NEXT_PUBLIC_API_BASE_URL=$(bashio::config 'api_base_url')
 fi
+
+# If NEXT_PUBLIC_API_BASE_URL is "null", then don't export it.
+if [[ "${NEXT_PUBLIC_API_BASE_URL}" == "null" ]]; then
+    unset NEXT_PUBLIC_API_BASE_URL
+    echo "NEXT_PUBLIC_API_BASE_URL is not set."
+    exit 1
+else
+    export NEXT_PUBLIC_API_BASE_URL
+    echo "NEXT_PUBLIC_API_BASE_URL=${NEXT_PUBLIC_API_BASE_URL}"
+fi
+
+npm run build
 
 npm run start
