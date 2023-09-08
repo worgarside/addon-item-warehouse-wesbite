4bef52424cc7a282ce4ca23271b863cf1a6f4f94
Will Garside <worgarside@gmail.com>
2023-09-08 21:36:43 +0100

    Fix issue with header icons not being set on page load

--- item_warehouse_website/app/components/Warehouse.client.tsx
+++ item_warehouse_website/app/components/Warehouse.client.tsx
@@ -41,6 +41,7 @@ const Warehouse: React.FC<WarehouseProps> = ({
   const {
     warehouseRefreshCount,
     getDisplayAsOptions,
+    currentWarehouseFieldOrder,
     updateWarehouseFieldOrder,
   } = useSettings();
   const warehouseDisplayOptions = getDisplayAsOptions(warehouseName);
@@ -48,6 +49,13 @@ const Warehouse: React.FC<WarehouseProps> = ({
 
   const [orderBy, setOrderBy] = useState<string | null>(null);
   const [ascending, setAscending] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    if (currentWarehouseFieldOrder) {
+      setOrderBy(currentWarehouseFieldOrder.fieldName);
+      setAscending(currentWarehouseFieldOrder.ascending);
+    }
+  }, [currentWarehouseFieldOrder]);
 
   const handleClick = (fieldName: string) => {
     let newOrderBy: string | null = null;
